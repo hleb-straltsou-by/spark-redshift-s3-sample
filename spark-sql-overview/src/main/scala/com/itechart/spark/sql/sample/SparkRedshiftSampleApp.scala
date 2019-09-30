bb0cafb78b6d075ee47bf405ae829c85904ab981
hleb.straltsou <hleb.straltsou@itechart-group.com>
2019-09-30 18:25:45 +0300

    Add writing features to S3 and Redshift

--- spark-sql-overview/src/main/scala/com/itechart/spark/sql/sample/SparkRedshiftSampleApp.scala
+++ spark-sql-overview/src/main/scala/com/itechart/spark/sql/sample/SparkRedshiftSampleApp.scala
@@ -3,30 +3,23 @@ package com.itechart.spark.sql.sample
 import com.itechart.spark.sql.constant.SparkConstants._
 import org.apache.spark.sql.functions._
 import com.itechart.spark.sql.utils.RedshiftUtils
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
 
 object SparkRedshiftSampleApp {
 
   def main(args:Array[String]): Unit = {
 
-    val spark = SparkSession
-      .builder()
-      .master("local")
-      .appName("Spark Redshift App")
-      .config("spark.hadoop.fs.s3a.access.key", awsAccessKeyId)
-      .config("spark.hadoop.fs.s3a.secret.key", awsSecretAccessKey)
-      .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-      .getOrCreate()
+    val masterUrl = "local"
+    val appName = "Spark Redshift Sample"
+    val spark = RedshiftUtils.getSparkSession(masterUrl, appName, awsAccessKeyId, awsSecretAccessKey)
 
-    val tempS3Dir = "s3a://redshift-training-01/"
+    val tempS3Dir = "s3a://redshift-training-01/temp"
 
     val queryForUsersAlphaSource = "SELECT * FROM USERS_SOURCE_ALPHA"
     val usersAlphaSourceDF = RedshiftUtils.getDF(spark, tempS3Dir, queryForUsersAlphaSource)
-    usersAlphaSourceDF.show
 
     val queryForUsersBetaSource = "SELECT * FROM USERS_SOURCE_BETA"
     val usersBetaSourceDF = RedshiftUtils.getDF(spark, tempS3Dir, queryForUsersBetaSource)
-    usersBetaSourceDF.show
 
     val schemaA = usersAlphaSourceDF.schema.toSet
     val schemaB = usersBetaSourceDF.schema.toSet
@@ -38,7 +31,19 @@ object SparkRedshiftSampleApp {
     }
 
     val resultDF = usersAlphaSourceDF.union(extendedDF)
-    resultDF.foreach(user => println(user))
+    //resultDF.foreach(user => println(user))
+    resultDF.show
+
+    resultDF.write
+      .mode(SaveMode.Overwrite)
+      .format("csv")
+      .save("output")
+
+    val outputS3Dir = "s3a://redshift-training-01/output"
+    RedshiftUtils.saveDFToS3(resultDF, outputS3Dir)
+
+    val saveTable = "USERS_RESULT"
+    RedshiftUtils.saveDFToRedshift(resultDF, saveTable, tempS3Dir)
 
     spark.stop()
   }
--- spark-sql-overview/src/main/scala/com/itechart/spark/sql/utils/RedshiftUtils.scala
+++ spark-sql-overview/src/main/scala/com/itechart/spark/sql/utils/RedshiftUtils.scala
@@ -1,9 +1,19 @@
 package com.itechart.spark.sql.utils
 
-import com.itechart.spark.sql.constant.SparkConstants.redshiftJdbcURL
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import com.itechart.spark.sql.constant.SparkConstants.{awsAccessKeyId, awsSecretAccessKey, redshiftJdbcURL}
+import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
 
 object RedshiftUtils {
+
+  def getSparkSession(masterUrl: String, appName: String, awsAccessKeyId: String, awsSecretAccessKey: String): SparkSession = {
+    SparkSession.builder()
+      .master(masterUrl)
+      .appName(appName)
+      .config("spark.hadoop.fs.s3a.access.key", awsAccessKeyId)
+      .config("spark.hadoop.fs.s3a.secret.key", awsSecretAccessKey)
+      .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+      .getOrCreate()
+  }
 
   def getDF(spark: SparkSession, tempS3Dir: String, query: String): DataFrame = {
      spark.read
@@ -13,6 +23,21 @@ object RedshiftUtils {
       .option("query", query)
       .option("forward_spark_s3_credentials", "true")
       .load()
+  }
+
+  def saveDFToS3(df: DataFrame, s3Dir: String): Unit = {
+    df.write.save(s3Dir)
+  }
+
+  def saveDFToRedshift(df: DataFrame, table: String, tempS3Dir: String): Unit = {
+    df.write
+      .format("com.databricks.spark.redshift")
+      .option("url", redshiftJdbcURL)
+      .option("dbtable", table)
+      .option("tempdir", tempS3Dir)
+      .option("forward_spark_s3_credentials", "true")
+      .mode(SaveMode.Append)
+      .save()
   }
 
 }
